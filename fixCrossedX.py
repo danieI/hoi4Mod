ce50f97362bb3f60a94aaaec34664714621673fa
danieI <daniellysack@gmail.com>
2023-02-09 14:41:37 -0600

    Add files via upload

--- fixCrossedX.py
+++ fixCrossedX.py
@@ -10,12 +10,153 @@ def unique(arr1,arr2):
         return False
     else:
         return True
+#------------------------------------------------------------------------------------------------------------
 
 def changeCol(x, y):
     colour = imgArr[x][y]
     imgArr[x+1][y] = colour
     imgArr[x][y+1] = colour
     imgArr[x+1][y+1] = colour
+#------------------------------------------------------------------------------------------------------------
+
+def overlap(first, second):
+    x1 = first[0]
+    x2 = first[0]
+    y1 = second[0]
+    y2 = second[1]
+
+    if any([(y1 > x1 and y1 < x2), (x1 > y1 and x1 < y2), x1 == y1, x1 == y2, x2 == y1, x2 == y2]):
+        return True
+    else:
+        return False
+#------------------------------------------------------------------------------------------------------------
+
+def edgeLines():
+    #get all [255,255,255] pixels that are the edge
+    edgeArr = []
+    for i in range(0,ymax-2):
+        for r in range(0,xmax-2):
+            if all(imgArr[i][r] == col):
+                pix = imgArr[i][r]
+                colArr.append([i,r])
+                ul = imgArr[i-1][r-1]
+                uu = imgArr[i-1][r]
+                ur = imgArr[i-1][r+1]
+                ll = imgArr[i][r-1]
+                rr = imgArr[i][r+1]
+                dl = imgArr[i+1][r-1]
+                dd = imgArr[i+1][r]
+                dr = imgArr[i+1][r+1]
+                fourSquare = [ul, uu, ur, ll, rr, dl, dd, dr]
+                sameArr = []
+                for sqr in fourSquare:
+                    sameArr.append(unique(pix, sqr))
+                if (any(sameArr)):
+                    edgeArr.append([i,r])
+
+    fileWriter = open("edgeArr.txt", "w")
+    for edge in edgeArr:
+        fileWriter.write(str(edge) + "/n")
+    fileWriter.close
+#------------------------------------------------------------------------------------------------------------
+
+def getHLines():
+    col = np.array([255,255,255]).astype('uint8')
+
+    #get all pixels that are [255,255,255]
+    colArr = []
+    #get all [255,255,255] pixels that are the edge
+    edgeArr = []
+    for i in range(0,ymax-2):
+        for r in range(0,xmax-2):
+            if all(imgArr[i][r] == col):
+                pix = imgArr[i][r]
+                colArr.append([i,r])
+                ul = imgArr[i-1][r-1]
+                uu = imgArr[i-1][r]
+                ur = imgArr[i-1][r+1]
+                ll = imgArr[i][r-1]
+                rr = imgArr[i][r+1]
+                dl = imgArr[i+1][r-1]
+                dd = imgArr[i+1][r]
+                dr = imgArr[i+1][r+1]
+                fourSquare = [ul, uu, ur, ll, rr, dl, dd, dr]
+                sameArr = []
+                for sqr in fourSquare:
+                    sameArr.append(unique(pix, sqr))
+                if (any(sameArr)):
+                    edgeArr.append([i,r])
+
+    #define horizontal lines
+    hLines = []
+    tempLine = []
+    for first in colArr:
+        second = colArr[colArr.index(first) + 1]
+        if first[1] + 1 == second[1]:
+            tempLine.append(first)
+            if colArr.index(second) + 1 == len(colArr):
+                tempLine.append(second)
+                hLines.append(tempLine)
+                break
+        else:
+            if(len(tempLine) > 0):
+                hLines.append(tempLine)
+            else:
+                hLines.append([first])
+            if colArr.index(second) + 1 == len(colArr):
+                break
+            tempLine = []
+
+
+    fileWriter = open("hlines.txt", "w")
+    for line in hLines:
+        fileWriter.write(str(line) + "/n")
+    fileWriter.close
+    fileWriter = open("edgeArr.txt", "w")
+    for edge in edgeArr:
+        fileWriter.write(str(edge) + "/n")
+    fileWriter.close
+    print("finished writing")
+    #------------------------------------------------------------------------------------------------------------
+def getVLines():
+    col = np.array([255,255,255]).astype('uint8')
+
+    #get all pixels that are [255,255,255]
+    colArr = []
+    for r in range(0,xmax-2):
+        for i in range(0,ymax-2):
+            if all(imgArr[i][r] == col):
+                colArr.append([i,r])
+
+    #define verticle lines
+    vLines = []
+    tempLine = []
+    for first in colArr:
+        second = colArr[colArr.index(first) + 1]
+        if first[0] + 1 == second[0]:
+            tempLine.append(first)
+            if colArr.index(second) + 1 == len(colArr):
+                tempLine.append(second)
+                vLines.append(tempLine)
+                break
+        else:
+            if(len(tempLine) > 0):
+                vLines.append(tempLine)
+            else:
+                vLines.append([first])
+            if colArr.index(second) + 1 == len(colArr):
+                break
+            tempLine = []
+
+
+    fileWriter = open("vlines.txt", "w")
+    for line in vLines:
+        fileWriter.write(str(line) + "/n")
+    fileWriter.close
+
+    print("finished writing")
+#------------------------------------------------------------------------------------------------------------
+
 
 
 def fixX():
@@ -100,75 +241,66 @@ def fixX():
 
 #need to change each shape of [255,255,255] into a random RGB colour and fix the border
 def replaceCol():
-    col = np.array([255,255,255]).astype('uint8')
-
-    #get all [255,255,255] pixels that are the edge
-    edgeArr = []
-    for i in range(0,ymax-2):
-        for r in range(0,xmax-2):
-            if all(imgArr[i][r] == col):
-                pix = imgArr[i][r]
-                ul = imgArr[i-1][r-1]
-                uu = imgArr[i-1][r]
-                ur = imgArr[i-1][r+1]
-                ll = imgArr[i][r-1]
-                rr = imgArr[i][r+1]
-                dl = imgArr[i+1][r-1]
-                dd = imgArr[i+1][r]
-                dr = imgArr[i+1][r+1]
-                fourSquare = [ul, uu, ur, ll, rr, dl, dd, dr]
-                sameArr = []
-                for sqr in fourSquare:
-                    sameArr.append(unique(pix, sqr))
-                if (any(sameArr)):
-                    edgeArr.append([i,r])
-
-    #define horizontal lines
-    hLines = []
-    tempLine = []
-    while(len(edgeArr) > 1):
-        first = edgeArr[0]
-        second = edgeArr[1]
-        if first[1] + 1 == second[1]:
-            tempLine.append(first)
-            if len(edgeArr) == 1:
-                tempLine.append(second)
-            edgeArr = edgeArr[1:]
-
-
-        else:
-            if(len(tempLine) > 0):
-                hLines.append(tempLine)
-            else:
-                hLines.append([first])
-            tempLine = []
-            edgeArr = edgeArr[1:]
 
 
 
-    #indexed array [x,yAvg]
+
+
+    #indexed array [x,yStart, yEnd, yAvg]
     posArr = []
     for line in hLines:
         yRange = [10000000000,0]
         for coord in line:
             if coord[1] < yRange[0]:
-                yRange[0] = coord
+                yRange[0] = coord[1]
             if coord[1] > yRange[1]:
-                yRange[1] = coord
+                yRange[1] = coord[1]
         x = line[0][0]
-        y = (yRange[0] + yRange[1]) / 2
-        posArr.append([x,y])
+        y = int((yRange[0] + yRange[1]) / 2)
+        posArr.append([x,yRange[0], yRange[1], y])
 
+    #seperate horizontal flat edges from verticle edges
 
 
     #need to seperate shapes from Lines
     posArrCopy = posArr
-    while len(posArr) > 1:
+    shapes = [[]]
+    midShape = False
+    firstLine = []
+    tempShape = []
+    i = 0
 
 
+    #print(hLines[0:25])
+    print(posArr[0:25])
 
-
-
+    while len(posArrCopy) > 1:
+        posArrCopy.sort(key = lambda x: (x[0],x[1]))
+        if midShape == False:
+            firstLine = posArrCopy[0]
+        possibleSecond = []
+        for line in posArrCopy:
+            if line[0] - 1 == firstLine[0]:
+                possibleSecond.append(line)
+        secondLine = possibleSecond[0]
+        for line in possibleSecond:
+            if abs(secondLine[3] - firstLine[3]) > abs(line[3] - firstLine[3]):
+                secondLine = line
+        print(firstLine)
+        print(secondLine)
+        if overlap([firstLine[1], firstLine[2]], [secondLine[1], secondLine[2]]) == False:
+            shapes.append(tempShape)
+            tempShape = []
+        elif overlap([firstLine[1], firstLine[2]], [secondLine[1], secondLine[2]]):
+            print("/n/ninside/n/n")
+            midShape = True
+            index = posArrCopy.index(firstLine)
+            posArrCopy = posArrCopy[0:index] + posArrCopy[index+1:]
+            firstLine = secondLine
+        i += 1
+        if i == 25:
+            break
+        print(len(posArrCopy))
 
 
 
@@ -194,4 +326,5 @@ def saveImg():
     finalImg.save('preovincesAltered.bmp')
 
 
-replaceCol()
+#replaceCol()
+getVLines()
